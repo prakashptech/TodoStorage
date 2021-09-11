d3539711b5b5f7f1ae3cc2c2793439623eb93f4a
prakashptech <prakashwithus97@gmail.com>
2021-09-10 19:52:28 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import List from "./components/List";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import 'font-awesome/css/font-awesome.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
